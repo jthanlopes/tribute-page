95e773356eb8d4c07bd90b26d89e06110eb4c697
Jonathan Santos <jonathanlpsantos@outlook.com>
2021-09-16 14:12:14 -0300

    Início da base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export function App() {
+	return(
+		<>
+			<main>
+				<header>
+	 				<h1>Aracy Guimarães Rosa</h1>
+	 				<p>A brasileira que salvou judeus do holocausto</p>
+	 			</header>
+			</main>
+		</>
+	);
+}
